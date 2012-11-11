8a624f19fcbc4e519124c0e52b0256f574d36906
Sebastien Fabbro <sfabbro@uvic.ca>
2012-11-11 11:00:29 -0800

    cholmod_config.h now generated at configure time

---
+++ addons/CHOLMOD/post-copy-hook.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# cholmod_config.h is generated at configure time
+mv Include/cholmod_config.h{,.orig}
+
---
+++ addons/CHOLMOD/post-install-hook.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# cholmod_config.h was generated at configure time
+mv Include/cholmod_config.h{.orig,}
