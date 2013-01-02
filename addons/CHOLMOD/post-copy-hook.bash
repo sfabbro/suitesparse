7dcb8a820b2d29974d6ee1fb7a736c7dabaca0a0
Sebastien Fabbro <sfabbro@uvic.ca>
2013-01-02 14:39:54 -0800

    mv Core to Cored to avoid conflicts in windows systems, thanks matzeri

--- addons/CHOLMOD/post-copy-hook.bash
+++ addons/CHOLMOD/post-copy-hook.bash
@@ -3,3 +3,5 @@
 # cholmod_config.h is generated at configure time
 mv Include/cholmod_config.h{,.orig}
 
+# cygwin: due to case insensitive file system "CHOLMOD/Core" collides with automake "rm -f core"
+mv Core Cored
--- addons/CHOLMOD/post-install-hook.bash
+++ addons/CHOLMOD/post-install-hook.bash
@@ -2,3 +2,6 @@
 
 # cholmod_config.h was generated at configure time
 mv Include/cholmod_config.h{.orig,}
+
+# cygwin: due to case insensitive file system "CHOLMOD/Core" collides with automake "rm -f core"
+mv Cored Core
